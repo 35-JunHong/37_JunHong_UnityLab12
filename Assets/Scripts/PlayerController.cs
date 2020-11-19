80a87cc8caa8c9e079c075c14eb2a68aa785a38c
JunHong30 <neojunhong2003@gmail.com>
2020-11-19 13:58:25 +0800

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject CoinCollected;
+    public float speed;
+
+    private int coinCount;
+    private int totalCoin;
+
+    float moveSpeed = 5.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalCoin = GameObject.FindGameObjectsWithTag("Coin").Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+            transform.rotation = Quaternion.Euler(0, 270, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+        }
+
+        //win
+        if (coinCount == totalCoin)
+        {
+            //print("You Win!");
+            SceneManager.LoadScene("WinScene");
+        }
+
+        //lose
+        if (transform.position.y < -5)
+        {
+            //print("You lose!");
+            SceneManager.LoadScene("LoseScene");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Coin")
+        {
+            coinCount++;
+
+            CoinCollected.GetComponent<Text>().text = "Coin Collected: " + coinCount;
+
+            Destroy(other.gameObject);
+        }
+    }
+}
